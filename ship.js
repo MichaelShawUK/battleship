223773ea5768009719b933ece6170aeff1736e6c
MichaelShawUK <MichaelShawUK@gmail.com>
2022-12-10 17:26:38 +0000

    Test ship factory function

--- ship.js
+++ ship.js
@@ -1,1 +1,28 @@
+const Ship = (length, start, orientation) => {
 
+  let hits = 0;
+
+  function getEndPoint(length, start, orientation) {
+    if (orientation === 'vertical') {
+      return [start[0], start[1] + length - 1]
+    } else if (orientation === 'horizontal') {
+      return [(start[0] + length - 1), start[1]]
+    }
+  }
+
+  return {
+    length,
+    start,
+    end: getEndPoint(length, start, orientation),
+    hits,
+    hit() {
+      this.hits++;
+    },
+    isSunk() {
+      if (this.hits === this.length) return true;
+      return false;
+    }
+  }
+}
+
+export default Ship;
--- ship.test.js
+++ ship.test.js
@@ -1,10 +1,22 @@
-test('1+1 = 2', () => {
-  expect(1+1).toBe(2);
+import Ship from './ship.js';
+
+test('Returns correct end-point when initialised vertically', () => {
+  expect(Ship(4, [5, 4], 'vertical').end).toEqual([5, 7]);
 })
 
-test('2+3 = 5', () => {
-  expect(2+3).toBe(5);
+test('Returns correct end-point when initialised horizontally', () => {
+  expect(Ship(4, [5, 4], 'horizontal').end).toEqual([8, 4]);
 })
 
+test('Increments hits property when hit method called', () => {
+  const destroyer = Ship(3, [4, 4], 'vertical');
+  destroyer.hit();
+  expect(destroyer.hits).toBe(1);
+})
 
-
+test('Determines whether ship has sunk', () => {
+  const patrolBoat = Ship(2, [1, 5], 'horizontal');
+  patrolBoat.hit();
+  patrolBoat.hit();
+  expect(patrolBoat.isSunk()).toBe(true);
+})
